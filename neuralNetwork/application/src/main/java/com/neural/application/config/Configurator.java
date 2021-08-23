cbb83fd2c41745f85aaa5b78bbab41253b88c44d
Serhii Naumenko <linkwow75@gmail.com>
2021-08-24 00:17:24 +0300

    prepare Configurator of application

--- neuralNetwork/application/src/main/java/com/neural/application/config/Configurator.java
+++ neuralNetwork/application/src/main/java/com/neural/application/config/Configurator.java
@@ -3,11 +3,9 @@ package com.neural.application.config;
 import com.libs.neuralcore.api.NeuralCoreAPI;
 import com.neural.application.service.NeuralService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.libs.neuralcore")
 public class Configurator {
 
     @Bean
