ff60e9831d44337cfef19b5df9d1cc626279ada8
Serhii Naumenko <linkwow75@gmail.com>
2021-08-24 00:23:42 +0300

    main Application was added

---
+++ neuralNetwork/application/src/test/java/com/neural/application/ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.neural.application;
+
+import com.neural.application.service.NeuralService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ComponentScan("com.libs.neuralcore")
+class ApplicationTests {
+
+    private NeuralService neuralService;
+
+    @Autowired
+    public void setNeuralService(NeuralService neuralService) {
+        this.neuralService = neuralService;
+    }
+
+    @Test
+    void contextLoads() {
+        Assertions.assertFalse(neuralService.createNeuralNetworkModel().isEmpty());
+    }
+}
